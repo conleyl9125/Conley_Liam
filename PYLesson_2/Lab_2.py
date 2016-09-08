af2b39ef006995fd3d8a01a4a7c081ca5b3cddc7
conleyl9125 <ConleyL9125@sduhsd.lan>
2016-09-08 09:49:28 -0700

    pylab2

---
+++ PYLesson_2/Lab_2.py
@@ -0,0 +1,16 @@
+x=8
+y=9
+print(x*y)
+
+name = "Liam"
+address = "13579"
+address2 = " Main Street."
+city = "San Diego"
+zipcode = "92130"
+print("Hi I am ",name,"and I live in",city,"on",address + address2,"My zipcode is ",zipcode," ")
+
+w = 37
+l = 54
+h = 76
+
+print((w * l + h * l + h * w) * 2)
