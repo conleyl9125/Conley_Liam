ab5fe8983173d0267de397540682e8a3369ef20a
conleyl9125 <ConleyL9125@sduhsd.lan>
2016-09-09 10:24:44 -0700

    Lesson_3

---
+++ PYLessson_3/RudeAI.py
@@ -0,0 +1,12 @@
+Python 3.2.5 (default, May 15 2013, 23:06:03) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Hi, my name is RudeAI. I'd like to ask you a few questions.
+What is your name?Liam
+Liam ?! hahahah!!
+How old are you Liam?15
+Ya whatever.
+What do you do for fun?moutain bike
+I thought only cool people moutain bike 
+>>> 
