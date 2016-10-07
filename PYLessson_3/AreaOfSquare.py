7a37938fd7ae01f1012aa84a23be2b6e1a1e2870
conleyl9125 <LTSoccerStud14@gmail.com>
2016-10-07 09:49:13 -0700

    fixed

---
+++ PYLessson_3/AreaOfSquare.py
@@ -0,0 +1,6 @@
+print ("I can also calculate any area of a square")
+length = int ( input ("What is the length of your square?"))
+width = int ( input ("What is the width of your squaer?"))
+print ("Your area of your square is",length*width,")
+
+
---
+++ PYLessson_3/BMIcalc.py
@@ -0,0 +1,4 @@
+print ("Now I am going to caluclate your most likely horrrible BMI")
+weight = int(input("How much do you weigh in pounds?"))
+height = int(input("How tall are you?"))
+print ("Your BMI is ",(703*(weight/(height*height))))
