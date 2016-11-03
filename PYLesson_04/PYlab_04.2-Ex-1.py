dad53d409baecf75e7a632e4e5e2d4f3236f71d2
conleyl9125 <LTSoccerStud14@gmail.com>
2016-11-03 10:10:10 -0700

    fixed

--- PYLesson_04/PYlab_04.2-Ex-1.py
+++ PYLesson_04/PYlab_04.2-Ex-1.py
@@ -3,6 +3,6 @@ l=float(input("What is the length in feet?"))
 w=float(input("What is the width in feet?"))
 
 def calcPerim(le,wi):
-    return(le*wi)
+    return((le+wi)*2)
 
 print("Your rectangle is", calcPerim(l,w) ,"ft around.")
