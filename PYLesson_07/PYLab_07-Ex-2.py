22c310291e9c53e041f0edfd3914fc02083766d2
liam conley <ltsoccerstud14@gmail.com>
2016-12-02 10:19:08 -0800

    9 and a fix

--- PYLesson_07/PYLab_07-Ex-2.py
+++ PYLesson_07/PYLab_07-Ex-2.py
@@ -1,13 +1,14 @@
-nu = int(input("Please enter a number"))
+number = int(input("Please enter a number"))
 average = 0
 digits = 0
-def avDigits(number):
-    global digits, average
+def avDigits():
+    global digits, average, number
     num = number
     while num > 0:
         digits += 1
-        average += digits % 10
+        average += (num % 10)
         num = int(num/10)
-    average /= 10
-avDigits(nu)
-print("The average of the digits in", nu ,"is",average )
+    average = average/digits
+avDigits()
+
+print("The average of the digits in", number ,"is",average)
---
+++ PYLesson_09/PYLab_09-Ex-1.py
@@ -0,0 +1,11 @@
+words = ["hey", "hi", "hello", "sup", "greetings"]
+print("in order...")
+for i in words:
+    print(words[0:4])
+print("")
+print("Reversed...")
+
+reverse()
+
+def reverse():
+    while 
