00c843fe64c07b0626fe434aaad9ef05a456239e
liam conley <ltsoccerstud14@gmail.com>
2017-01-03 10:25:01 -0800

    lab 10

---
+++ PYLesson_10/PYLab_10-Ex-1.py
@@ -0,0 +1,21 @@
+import random
+numsList=[]
+output = ""
+for i in range(0,4):
+    numsList.append(random.randint(1,100))
+print(numsList)
+
+numsList=[]
+for i in range(0,4):
+    numsList.append(random.randint(1,100))
+print(numsList)
+
+numsList=[]
+for i in range(0,4):
+    numsList.append(random.randint(1,100))
+print(numsList)
+
+numsList=[]
+for i in range(0,4):
+    numsList.append(random.randint(1,100))
+print(numsList)
---
+++ PYLesson_10/PYLab_10-Ex-2.py
@@ -0,0 +1,12 @@
+go=input("please enter 16 strings")
+
+spList = go.split(" ")
+
+wordsList=[]
+spot=0
+
+for i in range(0,4):
+    output=[]
+    for i in range(0,4):
+        spList[spot]+=wordsList
+print(wordsList)
