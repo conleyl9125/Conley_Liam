5f24bc497e1261ebfbe38d8af7fdeec6a39e6d15
liam conley <ltsoccerstud14@gmail.com>
2017-01-25 10:43:48 -0800

    new stuff

--- PYLesson_11/PYLab_11-Ex-2.py
+++ PYLesson_11/PYLab_11-Ex-2.py
@@ -2,17 +2,33 @@ import math
 class Distance:
     #Constructor:
     def __init__(self, x1="", y1="", x2="", y2=""):
-        self.xO = x1
-        self.yO = y1
-        self.xT = x2
-        self.yT = y2
-        ?
+        self.xO = int(x1)
+        self.yO = int(y1)
+        self.xT = int(x2)
+        self.yT = int(y2)
         distance = 0
 
     #modifier:
-        def setValues:
+    def setValues(self, x1="", y1="", x2="", y2=""):
+        self.xO = int(x1)
+        self.yO = int(y1)
+        self.xT = int(x2)
+        self.yT = int(y2)
+        distance = 0
         
 
     #Accessor:
-        def getMPH
-        
+    def getMPH():
+        distance = Math.sqrt((self.xTwo-self.xOne)*(self.xTwo-self.xOne)+(self.yTwo-self.yOne)*(self.yOne-self.yTwo));
+        return mph
+    
+    #main:
+    def main():
+        x1 = input("imput a value")
+        x2 = input("imput another value")
+        y1 = input("imput another value")
+        y2 = input("imput another value")
+
+
+    
+main()
---
+++ PYLesson_11/PYLab_11.1-Ex-1.py
@@ -0,0 +1,10 @@
+class Car:
+    #Constructor:
+    def __init__(self, p, i, e, t):
+
+    #Modifier:
+    def setCustom(p, i, e, t):
+        def __init__(self, p, i, e, t):
+
+    #Accessors
+    getPaint():
