f2d74f584b97210256edad4d146e31ef5e678042
liam conley <ltsoccerstud14@gmail.com>
2017-02-03 10:24:28 -0800

    website stuff

--- PYLesson_11/PYLab_11.2-Ex-6.py
+++ PYLesson_11/PYLab_11.2-Ex-6.py
@@ -1,28 +1,48 @@
 import random
-class User:
+class Item:
     #Constructor
-    def __init__(self, price, category, manufacturer, name = ""):
+    def __init__(self, price, category, manufa, UPC, name = ""):
+        self.Name = name
+        self.Manufa = manufa
         self.Price = price
+        self.Category = category
+        self.UPC = random.randint(100000000, 1000000000)
+
+    #Modifier
+    def setValues(self, price, category, manufa, UPC, name = ""):
+        self.Name = name
+        self.Manufa = manufa
+        self.Price = price
+        self.Category = category
         self.UPC = random.randint(100000000, 1000000000)
       
     #accessor
     def getPrice(self):
-        return self.price
+        return self.Price
     def getCategory(self):
-        return self.category
+        return self.Category
     def getManufacturer(self):
-        return self.lName
+        return self.Manufa
     def getName(self):
-        return self.userID
+        return self.Name
+    def getUPC(self):
+        return self.UPC
     def __str__(self):
-        return "Customer Info...\nPrice: " + self.price + \
-                           "\nCategory: " + self.category + \
-                           "\nName: " + self.name + \
-                           "\nMaunfacturer: " + str(self.maunfacturer)
+        return "Customer Info...\nPrice: " + self.Price + \
+                           "\nCategory: " + self.Category + \
+                           "\nName: " + self.Name + \
+                           "\nMaunfacturer: " + str(self.Manufa)
 
 def main():
-    maufacturer=input("What is the manufacturer?")
+    manufa=input("What is the manufacturer?")
     name=input("What is the name?")
-
+    yon=input("Will you be entering price and category information? (y/n)")
     
+    if yon=="n":
+        item1 = Item(category, manufa)
+    else:
+        price=input("What do you want the price to be?")
+        category=input("What is the category?")
+        item1 = Item(category, manufa, name, category, price)
+    print(item1)
 main()
